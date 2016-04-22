f6625fe53bddc43092972a2d10a4c23d13ee2bfe
Joshua Gantt <jr.gantt@icloud.com>
2016-04-22 18:09:40 -0500

    style start

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,10 @@
 
 module.exports = function(environment) {
   var ENV = {
+    googleFonts: [
+     'Questrial:400',
+   ],
+
     modulePrefix: 'nashville-free-park',
     environment: environment,
     baseURL: '/',
@@ -16,7 +20,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
   };
 
   if (environment === 'development') {
@@ -41,7 +45,6 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
 
-  }
-
+  };
   return ENV;
 };
