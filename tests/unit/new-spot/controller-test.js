9f79bb99b6c7845f15ef74c08ec414a4288e5a96
Daniel McBryde <mcbryded@gmail.com>
2016-04-22 14:30:38 -0500

    abort submit spot generation to avoid conflict

---
+++ tests/unit/new-spot/controller-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:new-spot', 'Unit | Controller | new spot', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
