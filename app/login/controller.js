7b348ff5f311447338607a2b2b6d8d7edca3f426
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-22 12:43:31 -0500

    User can login to site

---
+++ app/login/controller.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+
+  login({ email, password }) {
+    this.get(`session`).authenticate(`authenticator:application`, email, password)
+      .then(() => {
+        this.transitionToRoute(`index`);
+      })
+      .catch((reason) => {
+        console.log(reason);
+      });
+  },
+});
