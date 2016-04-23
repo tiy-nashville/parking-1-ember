a0f640e2115efc3ed0281f3c3c43dce82d541ae2
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-23 09:16:30 -0500

    Jf develop (#18)
    * User can login to site
    
    * Fix link to
    
    * Fixed login issues with Ryan
    
    * Create map view
    
    * Create map in map-view

--- app/mapview/controller.js
+++ app/mapview/controller.js
@@ -1,21 +1,75 @@
 import Ember from 'ember';
+import config from 'nashville-free-park/config/environment';
+
+function findPostCode(addressParts) {
+  return addressParts.reduce((carry, curr) => {
+    if (curr.types[0] === `postal_code`) {
+      return curr.long_name;
+    }
+
+    return carry;
+  }, null);
+}
 
 export default Ember.Controller.extend({
   lat: 36.174465,
   lng: -86.767960,
   zoom: 12,
-  emberparkspot1: [36.174455, -86.767890],
+  emberspot: [36.174455, -86.767890],
+  geolocateran: false,
 
-  geolocate() {
-    const street = `2704+linmar+avenue`;
-    const city = `Nashville`;
-    const state = `TN`;
+  address: {
+    street: ``,
+    city: ``,
+    state: ``,
+    zip: ``,
+  },
 
-    console.log(street, city, state);
-    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${street},${city}+${state}&key=AIzaSyCZc2PblaL6nbaQ9ds9hKu-bS7wb3HflTc`)
+  geolocate(address) {
+    Ember.set(this, `address`, address);
+    var rstreet = address.street.split(' ').join('+');
+    var rcity = address.city.split(' ').join('+');
+    var rstate = address.state.split(' ').join('+');
+
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${rstreet},${rcity}+${rstate}&key=${config.googleLeaflet.apiKey}`)
     .then((r) => r.json())
     .then((data) => {
-      console.log(data);
+      const components = data.results[0].address_components;
+      const { lat, lng } = data.results[0].geometry.location;
+      this.set(`address.zip`, findPostCode(components));
+      this.set(`emberspot`, [lat, lng]);
+      this.set(`geolocateran`, true);
+      this.set(`lat`, data.results[0].geometry.location.lat);
+      this.set(`lng`, data.results[0].geometry.location.lng)
     });
+  },
+
+  geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
   }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
 });
