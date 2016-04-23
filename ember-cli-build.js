0036d402383d3ef6bdde54a26b7a3ce2ed1737b5
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-23 16:25:32 -0500

    Update no fingerprint for deployment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,9 @@ module.exports = function(defaults) {
     babel: {
       includePolyfill: true,
     },
+    fingerprint: {
+      extensions: ['js', 'css', 'map'],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
